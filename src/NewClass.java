db3c2ccce73248e924b0250b8cdf955bb7d539d4
AbdullahIbin <abdullahibin@hotmail.com>
2019-09-17 21:24:00 -0400

    Update NewClass.java

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,6 +1,9 @@
 public class NewClass {
     public static void main(String[]args){
         System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
 
 
     }
